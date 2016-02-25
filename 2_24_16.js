dd53df5e6befa58adf3ee255af49da5b8db6d14f
Joseph Nolan <josephbnolan@me.com>
2016-02-25 09:48:20 -0600

    new toy problem

---
+++ 2_24_16.js
@@ -0,0 +1,20 @@
+/*
+write a function that will console log the numbers 1 through 5,
+with delays between each log equaling the logged number.
+delayLog() // pauses 1 second, then logs 1
+// pauses 2 second, then logs 2
+// pauses 3 seconds, then logs 3
+// pauses 4 seconds, then logs 4
+// pauses 5 seconds, then logs 5
+*/
+
+
+function delayLog() {
+  var counter = 1;
+
+  function logger() {
+    if (counter <= 5) {
+      setTimeout ()
+    }
+  }
+}
